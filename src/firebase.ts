369139d7e24ef0d9d5fe6cd2770635ea70e0bc01
sunny2you <ally1008@naver.com>
2024-03-23 19:25:02 +0900

    initialized firebase

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtYtUrpNIp76_H5ynwuJvwnqLNCEuq0b8",
+  authDomain: "nwitter-reloaded-9d3a5.firebaseapp.com",
+  projectId: "nwitter-reloaded-9d3a5",
+  storageBucket: "nwitter-reloaded-9d3a5.appspot.com",
+  messagingSenderId: "87262371870",
+  appId: "1:87262371870:web:f488da8d735bedbfec4e0f"
+};
+
+const app = initializeApp(firebaseConfig);
