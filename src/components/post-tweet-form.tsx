42a74958c95fcad68c0accb0f95e0bd25fa09f9d
sunny2you <ally1008@naver.com>
2024-03-27 08:46:27 +0900

    uploading images & code challenge(limiting file size)

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import styled from "styled-components"
-import { auth, db } from "../firebase";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form=styled.form`
     display: flex;
@@ -64,6 +65,7 @@ const SubmitBtn=styled.input`
 
 
 export default function PostTweetForm() {
+    const FILE_SIZE_MAX_LIMIT=1*1024*1024;
     const [isLoading, setLoading] =useState(false);
     const [tweet,setTweet]=useState("");
     const[file,setFile]=useState<File|null>(null); //it will be file or null
@@ -75,6 +77,10 @@ export default function PostTweetForm() {
     const onFileChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
         const { files } =e.target; //similar to create-account {target:{name,value}}
         if(files && files.length ===1){
+            if(files[0].size>FILE_SIZE_MAX_LIMIT){
+                alert("The maximum capacity that can be uploaded is 1mb");
+                return;
+            }
             setFile(files[0]);
         }
     };
@@ -85,13 +91,21 @@ export default function PostTweetForm() {
         if(!user||isLoading || tweet===""||tweet.length>180) return;
         try{
             setLoading(true);
-            await addDoc(collection(db,"tweets"),{
+            const doc=await addDoc(collection(db,"tweets"),{
                 tweet,
                 createdAt:Date.now(),
                 username: user.displayName || "Anonymous",
-                userId: user.uid, //삭제처리 위해서. 이름은 같을 수 있으니까. 
+                userId: user.uid, //삭제처리 위해서. 이름은 같을 수 있으니까.
 
             });
+            if(file){
+                const locationRef = ref(storage, `tweets/${user.uid}-${user.displayName}/${doc.id}`)
+                const result= await uploadBytes(locationRef, file); //uploadBytes(where, what)
+                const url= await getDownloadURL(result.ref);
+                await updateDoc(doc, {photo :url});
+            }
+            setTweet("");
+            setFile(null);
         }catch(e){}
         finally{
             setLoading(false);
@@ -99,7 +113,7 @@ export default function PostTweetForm() {
     }
     return   ( 
     <Form onSubmit={onSubmit}>
-        <TextArea rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?"/>
+        <TextArea required rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?"/>
         <AttachFileButton htmlFor="file">{file?"Photo added" :"Add Photo"}</AttachFileButton>  
         <AttachFileInput onChange={onFileChange}type="file" id="file" accept="image/*" /> 
         <SubmitBtn type="submit" value={isLoading?"Posting":"Post Tweet"}/>
