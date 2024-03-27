6076b80e07dfa724d6e2b7ac3f01562719259753
sunny2you <ally1008@naver.com>
2024-03-27 09:26:28 +0900

    Fetching Timeline

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 import PostTweetForm from "../components/post-tweet-form";
+import Timeline from "../components/timeline";
 
-const Wrapper=styled.div``;
+const Wrapper=styled.div`
+    display: grid;
+    gap:50px;
+    overflow-y: scroll;
+    grid-template-rows: 1fr 5fr;
+`;
 
 
 
@@ -10,6 +16,7 @@ export default function Home(){
     return (
         <Wrapper>
             <PostTweetForm/>
+            <Timeline/>
         </Wrapper>
     );
 }
